447a827379eef659b87adf3b6a47e89e6366ba88
Belen <lenchu.deanta@gmail.com>
2021-06-12 12:47:30 +0200

    logo

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,7 +6,11 @@ export function Header({ cart }) {
   return (
     <div className="header">
       <Link to="/landing" className="header__first">
-        <img className="header__logo" alt={"Logo"} />
+        <img
+          className="header__logo"
+          alt={"Logo"}
+          src="https://w7.pngwing.com/pngs/685/990/png-transparent-telephone-mobile-phone-accessories-nhyl-mobile-praxis-gabriel-logo-de-telefono-movil-rectangle-logo-grass.png"
+        />
       </Link>
       <span>Cart: {cart}</span>
     </div>
